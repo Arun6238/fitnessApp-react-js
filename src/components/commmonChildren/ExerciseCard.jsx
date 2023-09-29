95c0df858de904ee6cec4fecfc7a38be96bb8e81
Arun Suresh <arunsuresh241@gmail.com>
2023-09-29 13:04:53 +0530

    made some changes to ExerciseCard component

--- src/components/commmonChildren/ExerciseCard.jsx
+++ src/components/commmonChildren/ExerciseCard.jsx
@@ -1,47 +1,44 @@
 import {LazyLoadImage} from "react-lazy-load-image-component"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./exerciseCard.css"
 import { useState } from "react";
 
 export const ExerciseCard = ({ exercise,onClick}) => {
     const [loading,setLoading] = useState(true)
-    if (exercise != null) {
-        const isCustom = exercise.is_custom
-        return (
-            <div className="exercise-card" onClick={onClick}>
-                <div className="section-1">
-                    {loading?
-                        <div className="placeholder">
-                            {exercise.name[0]}
-                        </div>:
-                        null
-                    }
-                    {isCustom?
-                        null:
-                        <LazyLoadImage
-                            src={exercise.image}
-                            alt={`${exercise.name} image`}
-                            onLoad={()=>{
-                                setLoading(false)
-                            }}
-                        />
-                    }
-                </div>
-                <div className="section-2">
-                    <span className="exercise-name">
-                        {exercise.name}
-                    </span>
-                    <span className="exercise-bodypart">
-                        {exercise.body_part}
-                    </span>
-                </div>
+    const isCustom = exercise.is_custom
+    return (
+        <div className="exercise-card" onClick={onClick}>
+            <div className="section-1">
+                {loading?
+                    <div className="placeholder">
+                        {exercise.name[0]}
+                    </div>:
+                    null
+                }
+                {isCustom?
+                    null:
+                    <LazyLoadImage
+                        src={exercise.image}
+                        alt={`${exercise.name} image`}
+                        onLoad={()=>{
+                            setLoading(false)
+                        }}
+                    />
+                }
             </div>
-        );
-    }else {
-        return <ExerciseCardSkeleton/>;
-    }
+            <div className="section-2">
+                <span className="exercise-name">
+                    {exercise.name}
+                </span>
+                <span className="exercise-bodypart">
+                    {exercise.body_part}
+                </span>
+            </div>
+        </div>
+    );
 };
 
-const ExerciseCardSkeleton  = () => {
+export const ExerciseCardSkeleton  = () => {
    return <div className="exercise-card">
         <div className="section-1 skeleton"></div>
         <div className="section-2">
